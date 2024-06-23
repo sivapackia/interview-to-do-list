9b43ce9deab33d1665a0ee44f285088291ac0ac3
R.SIVAPACKIA <sivapackia2001@gmail.com>
2024-06-23 10:34:51 +0530

    updated

--- src/todolist/Router.js
+++ src/todolist/Router.js
@@ -6,7 +6,7 @@ import Home from "./Home/Home";
 const Router=()=>{
     return(
         <>
-        <BrowserRouter>
+        <BrowserRouter  basename={process.env.PUBLIC_URL}>
         <Routes>
             <Route path="/" element={<Home/>}></Route>
         </Routes>
